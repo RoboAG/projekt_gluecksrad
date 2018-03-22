5bcd3dc7100fb033cdc774b7cdd596f0ecf7f07f
Peter Weissig <peterweissig@github.com>
2018-03-22 11:27:27 +0100

    updated travis-job (echos name of current folder while building all tests)

--- travis/2_build_all.sh
+++ travis/2_build_all.sh
@@ -22,10 +22,12 @@ echo ""
 echo "## Tim"
 PATH_TIM="${PATH_DEVEL}Tim_atmeg88p_smd/"
 
+echo "# test_datenbus"
 cd ${PATH_TIM}test_datenbus/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# test_helligkeit"
 cd ${PATH_TIM}test_helligkeit/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
@@ -36,10 +38,12 @@ echo ""
 echo "## Richard"
 PATH_RICHARD="${PATH_DEVEL}Richard/"
 
+echo "# test_simple"
 cd ${PATH_RICHARD}test_simple/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# test_datenbus"
 cd ${PATH_RICHARD}test_datenbus/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
@@ -50,22 +54,27 @@ echo ""
 echo "## Alex"
 PATH_ALEX="${PATH_DEVEL}Alex/"
 
+echo "# buttons"
 cd ${PATH_ALEX}buttons/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# eeprom"
 cd ${PATH_ALEX}eeprom/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# LED_API_1"
 cd ${PATH_ALEX}LED_API_1/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# LED_API_2"
 cd ${PATH_ALEX}LED_API_2/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# timer"
 cd ${PATH_ALEX}timer/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
@@ -80,18 +89,22 @@ echo ""
 echo "## Peter"
 PATH_PETER="${PATH_DEVEL}Peter/tests/"
 
+echo "# leds1"
 cd ${PATH_PETER}leds1/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# leds2"
 cd ${PATH_PETER}leds2/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# cover"
 cd ${PATH_PETER}cover/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
 
+echo "# random"
 cd ${PATH_PETER}random/
 robolib_all
 if [ $? -ne 0 ]; then exit -1; fi
