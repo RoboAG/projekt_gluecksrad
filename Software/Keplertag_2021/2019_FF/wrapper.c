30529d96646aa8d17fd04887e501d196c5f17f32
Peter Weissig <peterweissig@github.com>
2021-09-27 01:30:25 +0200

    added old events

---
+++ Software/Keplertag_2021/2019_FF/wrapper.c
@@ -0,0 +1,24 @@
+/*******************************************************************************
+* wrapper.c                                                                    *
+* =========                                                                    *
+*                                                                              *
+* Version: 11.0.0                                                              *
+* Date   : 26.09.21                                                            *
+* Author : Peter Weissig, Alex Feilke                                          *
+*                                                                              *
+* See also:                                                                    *
+*   https://github.com/RoboAG/projekt_gluecksrad                               *
+*******************************************************************************/
+
+
+
+//*********************************<Constants>**********************************
+#define VERSION 11
+#define PRICES_MAX { 170, 70, 38, 11, 9 }
+
+
+
+//*********************************<Included files>*****************************
+// This file is just a simplified wrapper for different settings.
+// Therefore, we include the real "main.c" instead of any header.
+#include "../common/main.c"
